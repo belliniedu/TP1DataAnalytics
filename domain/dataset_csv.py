8b9caa93025bf1178774f4e4e6f547e4666566ee
belliniedu <belliniedu@gmail.com>
2025-06-06 20:44:38 -0300

    Se desarrollaron los archivos dataset_csv y dataset_json en domain

---
+++ domain/dataset_csv.py
@@ -0,0 +1,17 @@
+from domain.dataset import Dataset
+import pandas as pd
+
+
+class DatasetCSV(Dataset):
+    def __init__(self, fuente):
+        super().__init__(fuente)
+
+
+    def cargar_datos(self):
+        try:
+            df = pd.read_csv(self.fuente)
+            self.datos = df
+            if self.validar_datos():
+                self.transformar_datos()
+        except Exception as error:
+            print(f"Error cargando CSV: {error}")
